b4e0f6c7bb460404431df7559065fecfbe9ab56a
Ubuntu <ubuntu@ip-172-31-84-106.ec2.internal>
2021-02-19 20:01:44 +0000

    omitted XGB model

--- herg_docker.py
+++ herg_docker.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 from janitor.chemistry import morgan_fingerprint
 import pickle
-
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 act = {
-  1: "blocker",
-  0: "non-blocker",
+  1: "Blocker",
+  0: "Non-blocker",
 }
 
 # load the input molecule
@@ -27,18 +28,8 @@ y_pred_prob = np.round(y_pred_prob, 2)
 y_pred_prob = y_pred_prob[0]
 
 pkl_file.close()
-print('RF Pred:\t%s\t(%.2f)' % (act[y_pred], y_pred_prob))
 
-# XGB model
-pkl_file = open('xgb.pkl', 'rb')
-model = pickle.load(pkl_file)
-
-y_pred = model.predict(X_test)
-y_pred = y_pred[0]
-y_pred_prob = model.predict_proba(X_test).T[1]
-y_pred_prob = y_pred_prob.ravel()
-y_pred_prob = np.round(y_pred_prob, 2)
-y_pred_prob = y_pred_prob[0]
-
-pkl_file.close()
-print('XGB Pred:\t%s\t(%.2f)' % (act[y_pred], y_pred_prob))
+print('Input:\t%s' % 'Fc1ccc(cc1)Cn2c5ccccc5nc2NC4CCN(CCc3ccc(OC)cc3)CC4')
+print('Model:\t%s' % 'Random Forest')
+print('Pred:\t%s' % act[y_pred])
+print('Prob:\t%.2f' % y_pred_prob)
